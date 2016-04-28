4e3267872ea1deaddd11f927533943d93fb81c40
d.zemlyanitsyn <d.zemlyanitsyn@UK-RND-470>
2016-04-28 19:04:09 +0600

    init solutions

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            String a = Console.ReadLine();
+            Console.WriteLine("Введите ещё одно число");
+            String b = Console.ReadLine();
+            try
+            {
+                int c = Convert.ToInt32(a) + Convert.ToInt32(b);
+                Console.WriteLine("Сумма: " + (c).ToString());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Некорректные данные");
+            }
+        }
+    }
+}
