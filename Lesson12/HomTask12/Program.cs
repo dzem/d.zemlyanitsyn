20cccb4c3cc30084b1b86bddd065dc0150951eb5
dzem <dzem@outlook.com>
2016-06-14 00:23:18 +0700

    [Lesson12] ready for check

---
+++ Lesson12/HomTask12/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HomeTask12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создать коллекцию из 100 случайных дробей. Выбрать все дроби, которые являются целым числом. 
+            // Вывести на консоль их как целые числа.
+            try
+            {
+                Random random = new Random();
+
+                List<SimpleFraction> fractions = new List<SimpleFraction>();
+                for (int i=0; i <= 100; i++)
+                {
+                    fractions.Add(new SimpleFraction(random.Next(30), random.Next(1, 30)));
+                }
+
+                var ints = fractions.Where(f => f.Numerator % f.Denominator == 0)
+                    .Select(f => new
+                    {
+                        fractionInt = f.Numerator / f.Denominator
+                    });
+
+                Console.WriteLine("Целые числа:");
+
+                foreach(var numb in ints)
+                {
+                    Console.Write($"{numb.fractionInt} ");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson12/HomTask12/SimpleFraction.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace HomeTask12
+{
+    public struct SimpleFraction : IComparable
+    {
+        private long denominator;
+
+        public long Numerator { get; private set; }
+        public long Denominator
+        {
+            get { return denominator; }
+            private set
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentException("Знаменатель не может быть равен нулю");
+                }
+                if (value < 0)
+                {
+                    denominator = Math.Abs(value);
+                    Numerator = Numerator * -1;
+                }
+                else
+                {
+                    denominator = value;
+                }
+            }
+        }
+
+        public SimpleFraction(long numerator, long denominator) : this()
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+            Simplify();
+        }
+
+        private void Simplify()
+        {
+            long gcd = GCD(Numerator, Denominator);
+            Numerator = Numerator / gcd;
+            Denominator = Denominator / gcd;
+        }
+
+        private static long GCD(long a, long b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            for (;;)
+            {
+                long remainder = a % b;
+                if (remainder == 0) return b;
+                a = b;
+                b = remainder;
+            };
+        }
+
+        public static SimpleFraction operator +(SimpleFraction a, SimpleFraction b)
+        {
+            long gcd = GCD(a.Denominator, b.Denominator);
+
+            long num = a.Numerator * (b.Denominator / gcd) + b.Numerator * (a.Denominator / gcd);
+            long denom = a.Denominator * (b.Denominator / gcd);
+            return new SimpleFraction(num, denom);
+        }
+
+        public static SimpleFraction operator -(SimpleFraction a)
+        {
+            return new SimpleFraction(-a.Numerator, a.Denominator);
+        }
+
+        public static SimpleFraction operator -(SimpleFraction a, SimpleFraction b)
+        {
+            return a + -b;
+        }
+
+        public static SimpleFraction operator *(SimpleFraction a, SimpleFraction b)
+        {
+            return new SimpleFraction(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
+        }
+
+        public static SimpleFraction operator /(SimpleFraction a, SimpleFraction b)
+        {
+            if (b.Denominator == 0)
+            {
+                throw new DivideByZeroException("Нельзя делить на 0");
+            }
+            return a * new SimpleFraction(b.Denominator, b.Numerator);
+        }
+
+        public int CompareTo(object obj)
+        {
+            SimpleFraction value = (SimpleFraction)obj;
+            if (value.Numerator * Denominator < value.Denominator * Numerator)
+            {
+                return 1;
+            }
+            if (value.Numerator * Denominator > value.Denominator * Numerator)
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+    }
+
+}
+
