9072e7f08a9434f42cacae59eff175faff4f06bf
d.zemlyanitsyn <d.zemlyanitsyn@UK-RND-470>
2016-06-16 17:05:10 +0600

    [Lesson12] fix по замечаниям

--- Lesson12/HomTask12/Program.cs
+++ Lesson12/HomTask12/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace HomeTask12
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             // Создать коллекцию из 100 случайных дробей. Выбрать все дроби, которые являются целым числом. 
             // Вывести на консоль их как целые числа.
@@ -14,23 +13,21 @@ namespace HomeTask12
             {
                 Random random = new Random();
 
-                List<SimpleFraction> fractions = new List<SimpleFraction>();
-                for (int i=0; i <= 100; i++)
-                {
-                    fractions.Add(new SimpleFraction(random.Next(30), random.Next(1, 30)));
-                }
-
-                var ints = fractions.Where(f => f.Numerator % f.Denominator == 0)
+                var fractions = Enumerable.Range(0, 100)
+                    .Select(f => new SimpleFraction(random.Next(30), random.Next(1, 30)))
+                    .Where(f => f.Numerator % f.Denominator == 0)
                     .Select(f => new
                     {
+                        fraction = f.ToString(),
                         fractionInt = f.Numerator / f.Denominator
                     });
 
-                Console.WriteLine("Целые числа:");
+                Console.WriteLine("Целое число, дробь:");
 
-                foreach(var numb in ints)
+                foreach (var numb in fractions)
                 {
-                    Console.Write($"{numb.fractionInt} ");
+                    Console.Write($"{numb.fractionInt} , ");
+                    Console.WriteLine($"{numb.fraction}");
                 }
 
             }
--- Lesson12/Lesson12/Program.cs
+++ Lesson12/Lesson12/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lesson12
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Random random = new Random();
+
+            var ints = Enumerable.Range(0, 10)
+                .Select(i => random.Next(30))
+                .Where(i => i%2 == 0 || i > 5)
+                .OrderByDescending(i => i)
+                .Where(i =>
+                {
+                    Console.WriteLine(i);
+                    return true;
+                })
+                .ToArray();
+
+            Console.ReadKey();
         }
     }
 }
